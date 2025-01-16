ecb8bc4ad76fb916d662ac8c0bb2e02af79f4b0e
enkhiinbileg <artmongolian1@gmail.com>
2025-01-16 20:12:09 +0800

    first

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import React from 'react'
+import Home from './Page/Home'
+import EventPage from './Page/createEvent/EventPage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="flex items-center justify-center h-screen bg-gradient-to-br from-green-400 to-blue-600">
-      <h1 className="text-5xl font-bold text-white">Hello, Vite + Tailwind CSS!</h1>
-    </div>
+    <> 
+      <Home/>
+      <EventPage/>
+    </>
   )
 }
 
---
+++ src/component/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './hero.css'
+const Hero = () => {
+    return (
+        <div className='anime'>
+            <iframe src='https://my.spline.design/gaval-984ea93b4271d1dc763461a08a60a77f/' frameborder='0' width='100%' height='100%'></iframe>
+            <div className="dorvoljin"></div>
+            
+        </div>
+    );
+}
+
+export default Hero;
